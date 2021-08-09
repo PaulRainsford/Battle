a367e7e2f38c5fe4860a892b60c01c903d889be4
Paul Rainsford <paulrainsford69@gmail.co>
2021-08-09 17:21:47 +0100

    Added params

--- app.rb
+++ app.rb
@@ -13,10 +13,13 @@ get '/example' do
   'Bye!'
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Emma", "Almond"].sample
   erb(:index)
 end
 
-get '/visitor' do
-  erb "Hi there, Visitor!"
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
 end
